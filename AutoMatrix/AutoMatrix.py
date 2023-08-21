29874c5cc54677d1868c2a0421ac0e18991e3284
Leroux Gaelle <gaelleleroux974@gmail.com>
2023-08-21 09:52:18 -0400

    STYLE : add categorie parents to AutoMatrix

--- AutoMatrix/AutoMatrix.py
+++ AutoMatrix/AutoMatrix.py
@@ -28,7 +28,7 @@ class AutoMatrix(ScriptedLoadableModule):
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
         self.parent.title = "AutoMatrix"  # TODO: make this more human readable by adding spaces
-        self.parent.categories = ["Automated Dental Tools "]  # TODO: set categories (folders where the module shows up in the module selector)
+        self.parent.categories = ["Automated Dental Tools"]  # TODO: set categories (folders where the module shows up in the module selector)
         self.parent.dependencies = []  # TODO: add here list of module names that this module requires
         self.parent.contributors = ["Leroux Gaelle"]  # TODO: replace with "Firstname Lastname (Organization)"
         # TODO: update with short description of the module and a link to online module documentation
