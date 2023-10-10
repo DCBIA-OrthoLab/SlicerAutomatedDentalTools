23777279619de0f11865d472d68eafebf0dbc098
Leroux Gaelle <gaelleleroux974@gmail.com>
2023-10-10 17:08:46 -0400

    STYLE : change name parent.title

--- FlexReg/ButterfkyPatch.py
+++ FlexReg/ButterfkyPatch.py
@@ -40,7 +40,7 @@ class ButterfkyPatch(ScriptedLoadableModule):
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
         self.parent.title = "FlexReg"  # TODO: make this more human readable by adding spaces
-        self.parent.categories = ["Flex_Reg"]  # TODO: set categories (folders where the module shows up in the module selector)
+        self.parent.categories = ["Automated Dental Tools"]  # TODO: set categories (folders where the module shows up in the module selector)
         self.parent.dependencies = []  # TODO: add here list of module names that this module requires
         self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # TODO: replace with "Firstname Lastname (Organization)"
         # TODO: update with short description of the module and a link to online module documentation
