5a720d662b7eddce7fdbbfabfe783381db76bd27
GaelleLeroux <gaelleleroux974@gmail.com>
2023-11-01 11:57:14 -0400

    STYLE : delete print + comment

--- ALI_IOS/link.py
+++ ALI_IOS/link.py
@@ -6,41 +6,25 @@ import rpyc
 
 def call(name,args):
     home_dir = os.path.expanduser("~")
-    print("home_dir  : ",home_dir)
     default_install_path = "~/miniconda3"
     path_activate = "~/miniconda3/bin/activate"
     python_path_env = f"{home_dir}/miniconda3/envs/{name}/bin/python"
-    print("name  : ",{name})
     
-    # activate_env = os.path.join(default_install_path, "bin", "activate")
-    # python_executable = os.path.join(default_install_path, "envs",name,"python")  # Modifiez selon votre système d'exploitation et votre installation
-
 
     current_file_path = os.path.abspath(__file__)
-    print("current_file_path : ",current_file_path)
 
     # Répertoire contenant le script en cours d'exécution
     current_directory = os.path.dirname(current_file_path)
-    print("current_directory : ",current_directory)
 
-    # path_activate = os.path.join(default_install_path, "Scripts", "activate")
-    # activate_command = f"conda {path_activate} {name}"
-    # subprocess.run(activate_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8', errors='replace')
-    # Chemin absolu du fichier souhaité qui est à côté du script en cours d'exécution
-    
     # command = f"wsl -- bash -c \"source {path_activate} {name} && {python_path_env} {path_server}"
     path_server = os.path.join(current_directory, 'server3.py')
-    print("path_server : ",path_server)
     
     path_activate = f"{home_dir}/miniconda3/bin/activate"
     # command = f"/bin/bash -c 'source {path_activate} {name} && {python_path_env} {path_server}'"
-    print("name  : ",name)
-    print("-"*200)
+   
     command = f"/bin/bash -c 'source {path_activate} {name} && {python_path_env} {path_server} \"{sys.argv[1]}\" \"{sys.argv[2]}\" \"{sys.argv[3]}\" \"{sys.argv[4]}\" \"{sys.argv[5]}\" \"{sys.argv[6]}\"'"
     
-    print("w"*150)
-    print("command : ",command)
-    print("w"*150)
+  
     
      
     
@@ -54,38 +38,6 @@ def call(name,args):
     else:
         print(result.stdout)
         print("Process run succesfully")
-
-    # # Start server
-    # server_process = subprocess.Popen(command, shell=True)
-    
-    # # To be sure the server start
-    # time.sleep(5)
-    # print("on essaye de se connecter")
-    
-    # conn = rpyc.connect("localhost", 18817, config={"sync_request_timeout": 600})
-    # # wait_for_server_ready(conn)
-    # time.sleep(2)
-
-
-    # # print("on lui demande le resultat")
-    # # x=7
-    # # result=conn.root.running(x)
-    # # print(f"{x} au carre = {result}")
-    # print("args : ",args)
-    # conn.root.running(args)
-    # print(time.sleep(5))
-    
-    
-    # # Stop process
-    
-    # # server_process.terminate()
-    # # server_process.wait()
-    
-    # result = conn.root.stop()
-    # if result == "DISCONNECTING":
-    #     conn.close()
-
-    # print("on a ferme le server")
 
 
 
@@ -124,9 +76,7 @@ if __name__ == "__main__":
         # "sphere_radius": 0.3,
     }
         
-        print("args : ",args)
-        print("z"*150)
-        print(sys.argv)
+        
         name = sys.argv[7]
         
     else:
