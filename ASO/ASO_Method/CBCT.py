7cfca374d6dd858daa0a6fbfa0946e7815271a79
Jeanneclre <jeanne.claret@cpe.fr>
2023-12-18 13:18:57 -0500

    BUG: error importation ASO and init file in Automatrix

--- ASO/ASO_Method/CBCT.py
+++ ASO/ASO_Method/CBCT.py
@@ -1,5 +1,5 @@
-from Methode.Methode import Methode
-from Methode.Progress import DisplayASOCBCT, DisplayALICBCT
+from ASO_Method.Method import Methode
+from ASO_Method.Progress import DisplayASOCBCT, DisplayALICBCT
 import webbrowser
 import os
 import slicer
