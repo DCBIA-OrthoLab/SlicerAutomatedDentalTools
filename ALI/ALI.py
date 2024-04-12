3a029cad472e5be24c5cfb67b655443b768743c8
Leroux Gaelle <gaelleleroux974@gmail.com>
2024-04-12 17:40:10 -0400

    STYLE : change comment of msg box

--- ALI/ALI.py
+++ ALI/ALI.py
@@ -600,7 +600,7 @@ class ALIWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
   def onPredictButton(self):
     if platform.system()=="Windows" and not self.CBCT_as_input :
 
-      qt.QMessageBox.warning(self.parent, 'Warning', 'ALI_IOS is currently not available on Windows')
+      qt.QMessageBox.warning(self.parent, 'Warning', 'ALI_IOS is currently not available on Windows system on the version 5.6 of Slicer. Please use the Preview version of Slicer or a Linux system.')
       lib_ok = False
     else :
       lib_ok = install_function()
