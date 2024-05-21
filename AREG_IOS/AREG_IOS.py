2ec84261fde51e3fa20889722bc512579b614899
GaelleLeroux <gaelleleroux974@gmail.com>
2024-05-21 18:05:34 -0400

    ENH : areg running on windows

--- AREG_IOS/AREG_IOS.py
+++ AREG_IOS/AREG_IOS.py
@@ -1,106 +1,10 @@
 #!/usr/bin/env python-real
 
-
-# def installPackages():
-#     from slicer.util import pip_install, pip_uninstall
-#     import sys
-#     import os
-
-#     try:
-#         import pandas
-#     except ImportError:
-#         pip_install("pandas")
-
-#     try:
-#         import torch
-
-#         pyt_version_str = torch.__version__.split("+")[0].replace(".", "")
-#         version_str = "".join(
-#             [
-#                 f"py3{sys.version_info.minor}_cu",
-#                 torch.version.cuda.replace(".", ""),
-#                 f"_pyt{pyt_version_str}",
-#             ]
-#         )
-#         if version_str != "py39_cu113_pyt1120":
-#             raise ImportError
-#     except ImportError:
-#         # pip_install('--no-cache-dir torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113')
-#         pip_install(
-#             "--force-reinstall torch==1.12.0 torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113"
-#         )
-
-#     try:
-#         import monai
-#     except ImportError:
-#         pip_install("monai")
-
-#     from platform import system  # to know which OS is used
-
-#     if system() == "Darwin":  # MACOS
-#         try:
-#             import pytorch3d
-#         except ImportError:
-#             pip_install("pytorch3d")
-#             import pytorch3d
-
-#     else:  # Linux or Windows
-#         try:
-#             import pytorch3d
-
-#             if pytorch3d.__version__ != "0.7.0":
-#                 raise ImportError
-#         except ImportError:
-#             # try:
-#             # #   import torch
-#             #     pyt_version_str=torch.__version__.split("+")[0].replace(".", "")
-#             #     version_str="".join([f"py3{sys.version_info.minor}_cu",torch.version.cuda.replace(".",""),f"_pyt{pyt_version_str}"])
-#             #     pip_install('--upgrade pip')
-#             #     pip_install('fvcore==0.1.5.post20220305')
-#             #     pip_install('--no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/{version_str}/download.html')
-#             # except: # install correct torch version
-#             #     pip_install('--no-cache-dir torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113')
-#             #     pip_install('--no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py39_cu113_pyt1110/download.html')
-
-#             try:
-#                 code_path = os.sep.join(
-#                     os.path.dirname(os.path.abspath(__file__)).split(os.sep)
-#                 )
-#                 # print(code_path)
-#                 pip_install(
-#                     os.path.join(
-#                         code_path,
-#                         "AREG_IOS_utils",
-#                         "pytorch3d-0.7.0-cp39-cp39-linux_x86_64.whl",
-#                     )
-#                 )  # py39_cu113_pyt1120
-#             except:
-#                 pip_install(
-#                     "--force-reinstall --no-deps --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py39_cu113_pyt1120/download.html"
-#                 )
-
-#     try:
-#         import pytorch_lightning
-#     except ImportError:
-#         pip_install("pytorch_lightning==1.7.7")
-
-#     import numpy
-
-#     if float(".".join(numpy.__version__.split(".")[:2])) >= 1.23:
-#         pip_install("numpy==1.21.1")
-
-
 # installPackages()
 import os
 import sys
 import argparse
-
-import pandas
-import torch
-import monai
-import pytorch3d
-import pytorch_lightning
-import numpy
+import platform
 
 
 # from tqdm import tqdm
@@ -109,25 +13,45 @@ import numpy
 fpath = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(fpath)
 
-from AREG_IOS_utils import (
-    DatasetPatch,
-    PredPatch,
-    vtkMeshTeeth,
-    vtkICP,
-    ICP,
-    WriteSurf,
-    TransformSurf,
-)
+def check_platform():
+    if platform.system() == 'Windows':
+        return "Windows"
+    elif platform.system() == 'Linux':
+        if 'Microsoft' in platform.release():
+            return "WSL"
+        else:
+            return "Linux"
+    else:
+        return "Unknown"
+
+if check_platform()=="WSL":
+    from AREG_IOS_utils.dataset import DatasetPatch
+    from AREG_IOS_utils.PredPatch import PredPatch
+    from AREG_IOS_utils.vtkSegTeeth import vtkMeshTeeth
+    from AREG_IOS_utils.ICP import vtkICP
+    from AREG_IOS_utils.ICP import ICP
+    from AREG_IOS_utils.utils import WriteSurf
+    from AREG_IOS_utils.transformation import TransformSurf
+
+else : 
+    from AREG_IOS_utils import (
+        DatasetPatch,
+        PredPatch,
+        vtkMeshTeeth,
+        vtkICP,
+        ICP,
+        WriteSurf,
+        TransformSurf,
+    )
 
 
 def main(args):
-
     if not os.path.exists(os.path.split(args.log_path)[0]):
         os.mkdir(os.path.split(args.log_path)[0])
 
     with open(args.log_path, "w") as log_f:
         log_f.truncate(0)
-
+    
     dataset = DatasetPatch(args.T1, args.T2, "Universal_ID")
     Patched = PredPatch(args.model)
 
@@ -138,17 +62,21 @@ def main(args):
     lower = False
     if dataset.isLower():
         lower = True
+        
 
     # pbar = tqdm(total=len(dataset)*3,desc='Segment Palate')
     for idx in range(len(dataset)):
+        print("idx : ",idx)
 
         name = os.path.basename(dataset.getUpperPath(idx, "T1"))
+
         # pbar.set_description(f'Patch {name}')
         surf_T1 = dataset.getUpperSurf(idx, "T1")
         surf_T1 = Patched(dataset[idx, "T1"], surf_T1)
         # pbar.update(1)
 
         name = os.path.basename(dataset.getUpperPath(idx, "T1"))
+
         WriteSurf(surf_T1, args.output, name, args.suffix)
 
         with open(args.log_path, "r+") as log_f:
@@ -192,9 +120,7 @@ if __name__ == "__main__":
 
     print("Starting")
     print(sys.argv)
-
     parser = argparse.ArgumentParser()
-
     parser.add_argument("T1", type=str)
     parser.add_argument("T2", type=str)
     parser.add_argument("output", type=str)
@@ -203,5 +129,4 @@ if __name__ == "__main__":
     parser.add_argument("log_path", type=str)
 
     args = parser.parse_args()
-
     main(args)
