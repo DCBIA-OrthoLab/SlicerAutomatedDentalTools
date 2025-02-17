4a92ce9ee0ff89e77767d1bbe5724af2c735cdab
algadm <alban.gaydamour@cpe.fr>
2025-02-17 17:34:56 -0500

    ENH: first work on adding automatic approximation

---
+++ MRI2CBCT/MRI2CBCT_utils/Approx_MRI2CBCT.py
@@ -0,0 +1,97 @@
+from .Method import Method
+from .utils_CBCT import GetDictPatients, GetPatients
+import os, sys
+
+import SimpleITK as sitk
+import numpy as np
+
+from glob import iglob
+import slicer
+import time
+import qt
+import platform
+import re
+
+
+class Approximation_MRI2CBCT(Method):
+    def __init__(self, widget):
+        super().__init__(widget)
+        documentsLocation = qt.QStandardPaths.DocumentsLocation
+        documents = qt.QStandardPaths.writableLocation(documentsLocation)
+
+    def getGPUUsage(self):
+        if platform.system() == "Darwin":
+            return 1
+        else:
+            return 5
+
+    def NumberScan(self, scan_folder_t1: str, scan_folder_t2: str):
+        return len(GetDictPatients(scan_folder_t1, scan_folder_t2))
+    
+    def getModelUrl(self):
+        return {
+            "MeanCBCT": "xxx",
+            "ROI": "xxx"
+        }
+
+    def TestScan(self, scan_folder: str):
+        extensions = ['.nii', '.nii.gz']
+        found_files = self.search(scan_folder, extensions)
+        if any(found_files[ext] for ext in extensions):
+            return True, ""
+        else:
+            return False, "No files to run has been found in the input folder"
+        
+    def TestProcess(self, **kwargs) -> str:
+        out = ""
+        ok = True
+        
+        if kwargs["cbct_folder"] == "":
+            out += "Please select an input folder for CBCT scans\n"
+            ok = False
+            
+        if kwargs["mri_folder"] == "":
+            out += "Please select an input folder for MRI scans\n"
+            ok = False
+            
+        if kwargs["mean_folder"] == "":
+            out += "Please select an input folder for the mean CBCT\n"
+            ok = False
+            
+        if kwargs["ROI_file"] == "":
+            out += "Please select an input file for the ROI\n"
+            ok = False
+            
+        if kwargs["output_folder"] == "":
+            out += "Please select an output folder\n"
+            ok = False
+            
+        if out == "":
+            out = None
+
+        return ok,out
+    
+    def Process(self, **kwargs):
+        list_process=[]
+    
+        MRI2CBCT_APPROX = slicer.modules.mri2cbct_approx
+        parameter_mri2cbct_approx = {
+            "cbct_folder": kwargs["cbct_folder"],
+            "mri_folder": kwargs["mri_folder"],
+            "mean_folder": kwargs["mean_folder"],
+            "ROI_file": kwargs["ROI_file"],
+            "output_folder": kwargs["output_folder"],
+            "tempo_fold" : kwargs["tempo_fold"]
+        }
+        
+        list_process.append(
+            {
+                "Process": MRI2CBCT_APPROX,
+                "Parameter": parameter_mri2cbct_approx,
+                "Module": "Approximate files",
+            }
+        )
+           
+        return list_process
+    
+  
---
+++ MRI2CBCT_CLI/MRI2CBCT_APPROX/MRI2CBCT_APPROX.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python-real
+
+import argparse
+import os
+import re
+import shutil
+
+import sys
+fpath = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(fpath)
+from MRI2CBCT_CLI_utils import approximation, get_transformation, crop_volume
+
+def create_folder(folder):
+    """
+    Creates a folder if it does not already exist.
+
+    Arguments:
+    folder (str): Path of the folder to create.
+    """
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        
+def run_script_first_approximation(cbct_folder, mri_folder, output_folder):
+    """
+    Approximates CBCT images to MRI images and saves the resulting images.
+
+    Args:
+        cbct_folder (str): Path to the folder containing CBCT images
+        mri_folder (str): Path to the folder containing MRI images
+        output_folder (str): Path to the folder where output images will be saved
+
+    Returns:
+        str: Path to the folder containing the approximated images.
+    """
+    
+    first_approximation_folder = os.path.join(output_folder, "first_approximation")
+    create_folder(first_approximation_folder)
+    approximation(cbct_folder, mri_folder, first_approximation_folder)
+    return first_approximation_folder
+
+def run_script_get_transformation(mean_folder, cbct_folder, output_folder):
+    """
+    Generates the registration of the CBCT images to the mean CBCT and saves the results.
+
+    Args:
+        mean_folder (str): Path to the folder containing the mean CBCT image.
+        cbct_folder (str): Path to the folder containing the CBCT images.
+        output_folder (str): Path to the folder where the registered images will be saved.
+
+    Returns:
+        str: Path to the folder containing the registered images.
+    """
+    
+    transformation_folder = os.path.join(output_folder, "mean_registration")
+    create_folder(transformation_folder)
+    get_transformation(mean_folder, cbct_folder, transformation_folder)
+    return transformation_folder
+
+def delete_folder(folder_path):
+    """
+    Deletes a folder if it exists.
+
+    Arguments:
+    folder_path (str): Path of the folder to create.
+    """
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        print(f"The folder '{folder_path}' has been deleted successfully.")
+    else:
+        print(f"The folder '{folder_path}' does not exist.")
+        
+def run_script_crop_volumes(ROI_file, transformation_folder, first_approximation_folder, cbct_folder, output_folder):
+    """
+    Crops the CBCT volumes and MRI volumes based on the ROI and saves the results.
+
+    Args:
+        ROI_file (str): Path to the file containing the ROI.
+        transformation_folder (str): Path to the folder containing the transformation files.
+        first_approximation_folder (str): Path to the folder containing the approximated images.
+        mri_folder (str): Path to the folder containing the MRI images.
+        output_folder (str): Path to the folder where the cropped images will be saved.
+
+    Returns:
+        str: Path to the folder containing the cropped images.
+    """
+    
+    cropped_cbct_folder = os.path.join(output_folder, "cropped_cbct")
+    create_folder(cropped_cbct_folder)
+    crop_volume(ROI_file, transformation_folder, first_approximation_folder, cbct_folder, cropped_cbct_folder)
+
+def main():
+    parser = argparse.ArgumentParser(description="Run multiple Python scripts with arguments")
+    parser.add_argument('cbct_folder', type=str, help="Folder containing original CBCT images.")
+    parser.add_argument('mri_folder', type=str, help="Folder containing original MRI images.")
+    parser.add_argument('mean_folder', type=str, help="Folder containing the mean CBCT image.")
+    parser.add_argument('ROI_file', type=str, help="File containing the ROI.")
+    parser.add_argument('output_folder', type=str, help="Folder containing the outputs of the approximation.")
+    parser.add_argument('tempo_fold', type=str, help="Indicate to keep the temporary fold or not")
+    args = parser.parse_args()
+    
+    # Approximate MRI to CBCT
+    first_approximation_folder = run_script_first_approximation(args.cbct_folder, args.mri_folder, args.output_folder)
+
+    # Register CBCT to mean CBCT and get the transformation
+    transformation_folder = run_script_get_transformation(args.mean_folder, first_approximation_folder, args.output_folder)
+    
+    # Crop the volumes based on the ROI
+    run_script_crop_volumes(args.ROI_file, transformation_folder, first_approximation_folder, args.cbct_folder, args.output_folder)
+    
+    if args.tempo_fold=="false":
+        delete_folder(transformation_folder)
+
+if __name__ == "__main__":
+    print("Debug: MRI2CBCT_APPROX module is being loaded")
+
+    main()
