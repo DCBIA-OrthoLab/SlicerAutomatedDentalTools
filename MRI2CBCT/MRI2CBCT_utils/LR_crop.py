46f97b98905533e7b076c11f5f84f6a9bb04d7b4
algadm <alban.gaydamour@cpe.fr>
2025-05-16 15:21:34 -0400

    ENH: Added left padding and cropping of bilateral MRIs

--- MRI2CBCT/MRI2CBCT_utils/LR_crop.py
+++ MRI2CBCT/MRI2CBCT_utils/LR_crop.py
@@ -10,9 +10,10 @@ import slicer
 import time
 import qt
 import platform
+import re
 
 
-class Process_MRI(Method):
+class LR_CROP_MRI2CBCT(Method):
     def __init__(self, widget):
         super().__init__(widget)
         documentsLocation = qt.QStandardPaths.DocumentsLocation
@@ -26,32 +27,35 @@ class Process_MRI(Method):
 
     def NumberScan(self, scan_folder_t1: str, scan_folder_t2: str):
         return len(GetDictPatients(scan_folder_t1, scan_folder_t2))
-
-
+    
+    def getModelUrl(self):
+        return {
+        }
 
     def TestScan(self, scan_folder: str):
         extensions = ['.nii', '.nii.gz', '.nrrd']
-        found_files = self.search(scan_folder, extensions)
-        if any(found_files[ext] for ext in extensions):
-            return True, ""
-        else:
-            return False, "No files to run has been found in the input folder"
-        
+        if scan_folder!="None" :
+            found_files = self.search(scan_folder, extensions)
+            if any(found_files[ext] for ext in extensions):
+                return True, ""
+            else:
+                return False, "No files to run has been found in the "    
+        return True,""
         
     def TestProcess(self, **kwargs) -> str:
         out = ""
         ok = True
-
-        if kwargs["input_folder"] == "":
+        
+        if kwargs["input_folder_CBCT"] == "":
             out += "Please select an input folder for CBCT scans\n"
             ok = False
-
+            
+        if kwargs["input_folder_MRI"] == "":
+            out += "Please select an input folder for MRI scans\n"
+            ok = False
+            
         if kwargs["output_folder"] == "":
             out += "Please select an output folder\n"
-            ok = False
-
-        if kwargs["direction"] == "":
-            out += "Please select a new direction for X,Y and Z\n"
             ok = False
             
         if out == "":
@@ -61,20 +65,19 @@ class Process_MRI(Method):
     
     def Process(self, **kwargs):
         list_process=[]
-        # MRI2CBCT_ORIENT_CENTER_MRI
-        MRI2CBCT_ORIENT_CENTER_MRI = slicer.modules.mri2cbct_orient_center_mri
-        parameter_mri2cbct_orient_center_mri = {
-            "input_folder": kwargs["input_folder"],
-            "direction": kwargs["direction"],
-            "output_folder": kwargs["output_folder"],
-            "acquisition_z_spacing": kwargs["acquisition_z_spacing"],
+    
+        MRI2CBCT_LR_CROP = slicer.modules.mri2cbct_lr_crop
+        parameter_mri2cbct_lr_crop = {
+            "input_folder_CBCT": kwargs["input_folder_CBCT"],
+            "input_folder_MRI": kwargs["input_folder_MRI"],
+            "output_folder": kwargs["output_folder"]
         }
         
         list_process.append(
             {
-                "Process": MRI2CBCT_ORIENT_CENTER_MRI,
-                "Parameter": parameter_mri2cbct_orient_center_mri,
-                "Module": "Orientation and Centering of the MRI",
+                "Process": MRI2CBCT_LR_CROP,
+                "Parameter": parameter_mri2cbct_lr_crop,
+                "Module": "MRI2CBCT R/L Crop",
             }
         )
            
