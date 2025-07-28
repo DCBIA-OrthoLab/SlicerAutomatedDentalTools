961b2456f568fe1a6982e4007583edb733d9af37
lucieDLE <dolelucie0@gmail.com>
2025-07-28 13:41:49 -0400

    bug: fix pytorch install and upgrade to python 3.12

--- FlexReg/FlexReg_utils/install_pytorch.py
+++ FlexReg/FlexReg_utils/install_pytorch.py
@@ -12,7 +12,10 @@ def main(pip_path):
     ])
     print(version_str)
     # command = [pip_path, 'install',f'--no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/{version_str}/download.html']
-    command = [pip_path, 'install','--no-index', '--no-cache-dir' ,'pytorch3d', '-f', f'https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/{version_str}/download.html']
+    # command = [pip_path, 'install','--no-index', '--no-cache-dir' ,'pytorch3d', '-f', f'https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/{version_str}/download.html']
+    command = [pip_path, 'install','git+https://github.com/facebookresearch/pytorch3d.git']
+    print(command)
+
     result = subprocess.run(command,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True )
     print("Result : ",result.stdout)
     print("Error : ",result.stderr)
