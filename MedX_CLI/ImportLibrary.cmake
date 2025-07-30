7db5d45755c18efbe5636f531f3d459a8559b12b
algadm <alban.gaydamour@cpe.fr>
2025-07-29 23:25:22 -0400

    Updated CMake imports

--- MedX_CLI/ImportLibrary.cmake
+++ MedX_CLI/ImportLibrary.cmake
@@ -4,6 +4,8 @@ set(MODULE_NAME MedX_CLI_utils)
 #-----------------------------------------------------------------------------
 set(MODULE_PYTHON_SCRIPTS
   ${MODULE_NAME}/__init__.py
+  ${MODULE_NAME}/dashboard_utils.py
+  ${MODULE_NAME}/display_figure.py
   ${MODULE_NAME}/utils.py
 )
 
