59c105828649774ec9f02fc47f6be79cd60f1eb8
lucieDLE <dolelucie0@gmail.com>
2025-09-02 16:28:53 -0400

    enh: fix areg cbct fully automated scan check

--- AREG/AREG.py
+++ AREG/AREG.py
@@ -1029,14 +1029,14 @@ class AREGWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             monai_version = '==1.5.0' if sys.version_info >= (3, 10) else '==0.7.0'            
             if platform.system() == "Windows":
                 list_libs_CBCT_windows = [('itk','==5.4.0',None),('itk-elastix','==0.19.2',None),('dicom2nifti', '==2.3.0',None),('pydicom', '==2.2.2',None),('einops',None,None),('nibabel',None,None),('connected-components-3d','>=3.13.0',None),
-                            ('pandas',None,None),('torch','2.6.0',"https://download.pytorch.org/whl/cu118")] #(lib_name, version, url)
+                            ('pandas',None,None),('torch','<2.6.0',"https://download.pytorch.org/whl/cu118")] #(lib_name, version, url)
                 list_libs_CBCT_windows.append(('monai', monai_version, None))
 
                 is_installed = install_function(self,list_libs_CBCT_windows)
             else:
                 # libraries and versions compatibility to use AREG_CBCT
                 list_libs_CBCT = [('itk','==5.4.0',None),('itk-elastix','==0.19.2',None),('dicom2nifti', '==2.3.0',None),('pydicom', '==2.2.2',None),('einops',None,None),('nibabel',None,None),('connected-components-3d','>=3.13.0',None),
-                            ('pandas',None,None),('torch','2.6.0',None)] #(lib_name, version, url)
+                            ('pandas',None,None),('torch','<2.6.0',None)] #(lib_name, version, url)
 
                 list_libs_CBCT.append(('monai', monai_version, None))
 
